0d937e7ecb44c9c5202b4aa3ce244def5808c77e
Chinmay-n48 <chinmaynale01@gmail.com>
2026-05-07 22:09:21 +0530

    Added the Utils package and in that action class

---
+++ Selenium_OrangeHRM_WebAppFramework/src/main/java/utils_orangeHRM/OHWaitUtils.java
@@ -0,0 +1,10 @@
+package utils_orangeHRM;
+
+import org.openqa.selenium.WebDriver;
+
+public class OHWaitUtils {
+	WebDriver driver;
+	
+	
+
+}
--- Selenium_OrangeHRM_WebAppFramework/src/test/java/tests_orangeHRM/OHLoginPageTests.java
+++ Selenium_OrangeHRM_WebAppFramework/src/test/java/tests_orangeHRM/OHLoginPageTests.java
@@ -1,5 +1,7 @@
 package tests_orangeHRM;
 
+import org.testng.annotations.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -10,10 +12,12 @@ import com.saucedemo.utils.ActionUtils;
 
 import base_orangeHRM.OHBaseTest;
 import pages_orangeHRM.OHLoginPage;
+import utils_orangeHRM.OHActionUtils;
 
 public class OHLoginPageTests extends OHBaseTest {
 	
 	OHLoginPage login;
+	OHActionUtils action;
 
 	    @BeforeMethod
 	    public void setupPage() {
@@ -21,8 +25,14 @@ public class OHLoginPageTests extends OHBaseTest {
 	    }
 
 	    @Test
-		//Verify login with valid credentials
-		public void TC_OH_LoginPage_001()  {
-			login.Login("Admin","admin123");
-		}
+		//Verify login page loads successfully
+		public void TC_Login_UI_001()  {
+			//login.Login("Admin","admin123");
+			String urlText= driver.getCurrentUrl();
+			System.out.println(urlText);
+			Assert.assertEquals("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index", urlText, "Url is not Matched");
+			boolean b=action.isDisplayed(driver, login.UsernameField);
+            Assert.assertTrue(b , "Username Field is not available");
+            
+	    }
 }
